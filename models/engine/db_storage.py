f12f25c86c2b43db5463165d843fe1ed52b68de6
omarIdhmaid <o.idhmaid@gmail.com>
2023-12-11 00:15:15 +0100

    db env for python

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,9 +2,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from models.base_model import Base, BaseModel
+from models.user import User
 
 classes = {
-    "BaseModel": BaseModel
+    "BaseModel": BaseModel,
+    "User": User
 }
 
 
@@ -14,13 +16,13 @@ class DBStorage:
     __session = None
 
     def __init__(self):
-        db_url = "postgresql://admin:admin@127.0.0.1:5432/cms"
+        db_url = "postgresql://postgres:root@127.0.0.1:5432/cms"
         self.__engine = create_engine(db_url)
 
     def all(self):
         """query on the current database session"""
         new_dict = {}
-        objs = self.__session.query(BaseModel).all()
+        objs = self.__session.query(User).all()
         for obj in objs:
             key = obj.__class__.__name__ + '.' + obj.id
             new_dict[key] = obj
@@ -49,4 +51,3 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
-
