fd52f48a368f6110c88cf858a6332a2f14c7e636
badrxd <badrxdxd@gmail.com>
2023-12-10 20:19:26 +0100

    add init methode

---
+++ models/user.py
@@ -0,0 +1,14 @@
+"""This module defines a class User"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Boolean
+
+
+class User(BaseModel, Base):
+    """This class defines a user by various attributes"""
+    __tablename__ = 'users'
+    userName = Column(String(128), nullable=False, unique=True)
+    password = Column(String(128), nullable=False)
+    fullName = Column(String(60), nullable=False)
+    isBlocked = Column(Boolean, nullable=False, default=False)
+    role = Column(String(60), nullable=False)
+    secretKey = Column(String(60), nullable=False, unique=True)
