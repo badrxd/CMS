9648661446f6558835214c57d90b877a764abd7a
omarIdhmaid <o.idhmaid@gmail.com>
2023-12-18 18:11:53 +0100

    app no tmp frames

--- app/view/app.py
+++ app/view/app.py
@@ -12,38 +12,41 @@ ctk.set_default_color_theme("blue")
 
 
 class App(ctk.CTk):
-    _width = 0
-    _height = 0
+    __width = 0
+    __height = 0
 
     def __init__(self):
         super().__init__()
         self.title("CMS")
-        self._width = self.winfo_screenwidth() - 100
-        self._height = self.winfo_screenheight() - 100
-        self.geometry(f"{self._width}x{self._height}")
+        self.width = self.winfo_screenwidth() - 100
+        self.height = self.winfo_screenheight() - 100
+        self.geometry(f"{self.__width}x{self.__height}")
 
         self.frames = {}
         self.notif = None
 
         # Initialize frames
-        self.frames["LoginFrame"] = LoginFrame(self, self.show_dashboard)
-        self.frames["DashboardFrame"] = DashboardFrame(self, self.show_login)
+        # self.frames["DashboardFrame"] = DashboardFrame(self, self.show_login)
+        # self.frames["LoginFrame"] = LoginFrame(self, self.show_dashboard)
 
         # Show the initial frame
         self.show_login()
 
     def show_login(self):
-        self.frames["DashboardFrame"].pack_forget()
+        self.frames["LoginFrame"] = LoginFrame(self, self.show_dashboard)
+        if "DashboardFrame" in self.frames:
+            self.frames["DashboardFrame"].destroy()
         self.frames["LoginFrame"].place(
             relx=0.5, rely=0.5, anchor="center")
 
     def show_dashboard(self):
-        self.frames["LoginFrame"].place_forget()
+        self.frames["DashboardFrame"] = DashboardFrame(self, self.show_login)
+        self.frames["LoginFrame"].destroy()
         self.frames["DashboardFrame"].pack()
 
     def notification(self, txt):
         self.notif = ctk.CTkLabel(
             self, text=txt, corner_radius=7, fg_color="white"
         )
-        
+
         self.notif.pack(pady=(50, 10), padx=20)
