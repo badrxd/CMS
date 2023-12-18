9fa009ad27e77e833706dcab1c69e6eec4c55941
omarIdhmaid <o.idhmaid@gmail.com>
2023-12-18 05:04:03 +0100

    app + login + dash

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
-from app.gui.main_window import Login, Dashboard
-# from app import app
+from app.view.app import App
 
-
-# def user_login(self):
-#     """"""
-#     app.notification("good")
-#     app.login_frame.configure()
-
-
-def run_dash(prev=""):
-    dash = Dashboard(func[prev])
-    dash.mainloop()
-
-
-def run_logic(next=""):
-    login = Login(250, 300, func[next])
-    login.mainloop()
-
-
-func = {"dash": run_logic, "log": run_dash}
 
 if __name__ == "__main__":
-    run_logic("log")
+    app = App()
+    app.mainloop()
