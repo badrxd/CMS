02ba5dcc86075e4507a23baafaf778f1d950f5a8
badrxd <badrxdxd@gmail.com>
2023-12-27 01:07:29 +0100

    chnage schema, and add logic for car and customer

--- app/core/image_handler.py
+++ app/core/image_handler.py
@@ -2,22 +2,31 @@ import shutil
 import os
 
 
-def ImageName(key, path, name=None):
+def ImageName(name, path):
     file_name = os.path.basename(path)
     extention = file_name.split('.')[-1]
-    name = ''
-    if key == 'card_id_image' or key == 'driver_id_image':
-        words = key.split('_')
-        name = f'{words[0]}-{words[1]}.{extention}'
-    return name.capitalize()
+    img_name = ''
+    if name == 'card_id_image' or name == 'driver_id_image':
+        words = name.split('_')
+        img_name = f'{words[0]}-{words[1]}.{extention}'
+    else:
+        img_name = f'{name}.{extention}'
+    return img_name.capitalize()
 
 
-def SaveImage(file_path, section, id, path, img_name):
-    destination_path = os.path.join(path, section, id, img_name)
+def SaveImage(file_path, section, path, img_name, id=''):
+    destination_path = ''
     os.makedirs(os.path.join(path, section, id,), exist_ok=True)
     try:
         if section == 'customers':
-            shutil.copyfile(file_path, destination_path)
+            destination_path = os.path.join(path, section, id, img_name)
+        elif section == 'cars':
+            destination_path = os.path.join(path, section, img_name)
+        else:
+            pass
+
+        shutil.copyfile(file_path, destination_path)
+
         return ({'error': False})
     except Exception as e:
         print(e)
--- models/car.py
+++ models/car.py
@@ -10,9 +10,9 @@ class Car(BaseModel, Base):
 
     __tablename__ = 'cars'
     brand = Column(String(60), nullable=False)
-    image = Column(String(255), nullable=False)
+    car_image = Column(String(255), nullable=False)
     matricule = Column(String(60), nullable=False)
     rent_price = Column(Integer, nullable=False)
-    currency = Column(String(60), nullable=False)
+    currency = Column(String(10), nullable=False, default="DH")
     availability = Column(Boolean, nullable=False, default=True)
     reservations = relationship('Reservation', backref='car')
